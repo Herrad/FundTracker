1604e95b8ad554970e544f94a3a81b6e0b763e5c
unknown <herrad.koe@gmail.com>
2013-09-19 21:13:16 +0100

    Tests for index

--- Test.FundTracker.Web/Controllers/Home/TestHomeController.cs
+++ Test.FundTracker.Web/Controllers/Home/TestHomeController.cs
@@ -1,3 +1,5 @@
+using System.Web.Mvc;
+using FuncTracker.Web.Controllers;
 using NUnit.Framework;
 
 namespace Test.FundTracker.Web.Controllers.Home
@@ -5,6 +7,13 @@ namespace Test.FundTracker.Web.Controllers.Home
     [TestFixture]
     public class TestHomeController
     {
-         
+        [Test]
+        public void Index_returns_ViewResult_with_empty_ViewName()
+        {
+            var homeController = new HomeController();
+            var viewResult = homeController.Index();
+
+            Assert.That(viewResult.ViewName, Is.EqualTo(string.Empty));
+        }
     }
 }

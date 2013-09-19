19fcaa376470aabce8956734183584a8b343d69e
unknown <herrad.koe@gmail.com>
2013-09-19 21:31:19 +0100

    New view and controller for creating wallet

---
+++ FuncTracker.Web/Controllers/WalletController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FuncTracker.Web.Controllers
+{
+    public class WalletController : Controller
+    {
+        //
+        // GET: /Wallet/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}

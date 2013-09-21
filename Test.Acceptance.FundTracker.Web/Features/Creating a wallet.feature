1a57c35e67dfc292b5b598426bd0951a525913ec
unknown <herrad.koe@gmail.com>
2013-09-21 13:26:17 +0100

    Finished first acceptance test and added gemfile

---
+++ Test.Acceptance.FundTracker.Web/Features/Creating a wallet.feature
@@ -0,0 +1,8 @@
+﻿Feature: Creating a wallet
+	In order to store money
+	As someone with incoming and outgoing transactions
+	I want to be able to create a wallet
+
+Scenario: Creating a wallet with a name
+	When I create a wallet with the name "my first wallet"
+	Then I am taken to the display wallet page
---
+++ Test.Acceptance.FundTracker.Web/Steps/CreatingAWalletSteps.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace Test.Acceptance.FundTracker.Web.Steps
+{
+    [Binding]
+    public class CreatingAWalletSteps : ChromeDriverTest
+    {
+        [When(@"I create a wallet with the name ""(.*)""")]
+        public void WhenICreateAWalletWithTheName(string walletName)
+        {
+            var nameBox = _chromeDriver.FindElementByName("name");
+            nameBox.SendKeys(walletName);
+
+            var createButton = _chromeDriver.FindElementByClassName("button");
+            createButton.Click();
+
+            var redirectLink = _chromeDriver.FindElementByPartialLinkText("Click here");
+            redirectLink.Click();
+        }
+
+        [Then(@"I am taken to the display wallet page")]
+        public void ThenIAmTakenToTheDisplayWalletPage()
+        {
+            var pageTitle = _chromeDriver.FindElementByTagName("h2").Text;
+            Assert.That(pageTitle, Is.EqualTo("Wallet"));
+        }
+    }
+
+    public class ChromeDriverTest
+    {
+        protected static ChromeDriver _chromeDriver;
+
+        [BeforeScenario]
+        public void SetUpSeleniumOnChrome()
+        {
+            _chromeDriver = new ChromeDriver("WebDriver") {Url = "localhost:27554"};
+            _chromeDriver.Navigate();
+        }
+
+        [AfterScenario]
+        public void TearDownChromeDriver()
+        {
+            _chromeDriver.Close();
+            _chromeDriver.Quit();
+        }
+    }
+}

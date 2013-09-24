1f24124d2569055f71b35f8780a75931db13e3ed
unknown <herrad.koe@gmail.com>
2013-09-24 22:30:21 +0100

    removed deploy task from rake

--- rakefile.rb
+++ rakefile.rb
@@ -27,22 +27,3 @@ nunit :acceptance => :build do |nunit|
 	nunit.assemblies = @acceptance_assemblies
 end
 
-task :deploy => [:build, :test] do
-	
-	FileUtils.rm_rf("#{@deploy_target}/*") if Dir.exists?(@deploy_target)
-	
-	FileUtils.mkdir(@deploy_target) unless Dir.exists?(@deploy_target)
-	FileUtils.chmod("a=rwx", @deploy_target)
-
-	puts "Deploying to #{@deploy_target}"
-
-	Dir.glob("FundTracker.Web/*").each do |file|
-		dir, filename = File.dirname(file), File.basename(file)
-		dest = File.join(@deploy_target, dir)
-		FileUtils.mkdir(dest) unless Dir.exists?(dest)
-		FileUtils.cp_r(file, File.join(dest,filename))
-	end
-	
-	puts "Deployed"
-end
-
